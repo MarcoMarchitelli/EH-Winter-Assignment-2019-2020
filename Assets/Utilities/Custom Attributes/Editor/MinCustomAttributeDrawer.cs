390b0aca9c17a1abe684fd1fe9cc4a4a0fd5e8cd
MarcoMarchitelli <igvvcdhk@gmail.com>
2020-01-17 16:49:11 +0100

    Custom min attribute.

---
+++ Assets/Utilities/Custom Attributes/Editor/MinCustomAttributeDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer( typeof( MinCustomAttribute ) )]
+public class MinCustomAttributeDrawer : PropertyDrawer {
+    public override void OnGUI ( Rect position, SerializedProperty property, GUIContent label ) {
+        MinCustomAttribute mca = attribute as MinCustomAttribute;
+
+        if ( property.propertyType == SerializedPropertyType.Integer ) {
+            if ( property.intValue < mca.intValue )
+                property.intValue = mca.intValue;
+        }
+        else if ( property.propertyType == SerializedPropertyType.Float ) {
+            if ( property.floatValue < mca.floatValue )
+                property.floatValue = mca.floatValue;
+        }
+
+        EditorGUI.PropertyField( position, property, label );
+    }
+}
---
+++ Assets/Utilities/Custom Attributes/MinCustomAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[AttributeUsage( AttributeTargets.Field )]
+public class MinCustomAttribute : PropertyAttribute {
+    public int intValue;
+    public float floatValue;
+
+    public MinCustomAttribute ( int value ) {
+        intValue = value;
+    }
+
+    public MinCustomAttribute ( float value ) {
+        floatValue = value;
+    }
+}
